55117d9c7b15cf3a76d7190550e41ab7cae9cdb2
deyvi2003 <135271755+deyvi2003@users.noreply.github.com>
2025-11-03 14:40:14 -0500

    Update vertex.mjs

--- vertex.mjs
+++ vertex.mjs
@@ -930,4 +930,5 @@ app.server = app.listen(port, () => {
 app.server.on('upgrade', (req, socket, head) => {
   if (!req.url || !req.url.startsWith('/twilio')) { socket.destroy(); return; }
   wss.handleUpgrade(req, socket, head, (ws) => wss.emit('connection', ws, req));
+  a
 });
